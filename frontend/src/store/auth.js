55b84cdb9ccaed54ca006446fbed1cdd15e7abb1
Vaibhavi <vaibhaviagrawal466@gmail.com>
2025-07-06 15:50:02 +0530

    added  conditionally render navbar links and auth buttons based on login state using Redux

---
+++ frontend/src/store/auth.js
@@ -0,0 +1,27 @@
+// src/store/authSlice.js
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoggedIn: false,
+  role: "user",
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+    changeRole(state, action) {
+      state.role = action.payload;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+export default authSlice.reducer;
