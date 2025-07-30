a2315a89f596b791cfc72e1e64106807329c021a
unknown <pushpalata3083@gmail.com>
2025-07-30 21:52:04 +0530

    cart feature added

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,14 +1,70 @@
 import React ,{useState} from "react";
 import Loader from "../components/Loader/Loader";
+import axios from "axios";
+import{AiFillDelete} from "react-icons/ai";
 const Cart=()=>{
   const [Cart,setCart]=useState(); //by defalut cart is empty
+  const [Total,setTotal]=useState(0);
+  const headers={
+    id:localStorage.getItem("id"),
+    authorization:'Bearer ${localStorage.getItem("token")}',
+  };
+  useEffect(()=>{
+    const fetch =async()=>{
+      const res=await axios.get(
+        "http:localhost:1000/api/v1/get-user-cart",
+        {headers}
+      );
+      setCart(res.data.data);
+    };
+    fetch();
+  },[Cart]);
+
+  const deleteItem =async(bookid)=>{
+    const response =await axios.put(
+      `http://localhost:1000/api/v1/get-user-cart/${bookid}` ,
+      {},
+      {headers}
+    );
+    alert(response.data.message);
+  };
+
+  useEffect(()=>{
+    if(Cart && Cart.length >0){
+      let total=0;
+      Cart.map((items)=>{  //iteration on the map
+        total += items.price;
+      });
+      setTotal(total);
+      total=0;
+    }
+  },[Cart]);
+
+  const PlaceOrder =async()=>{
+    try{
+      const response=await axios.post(
+        `http://localhost:1000/api/v1/place-order`,
+        {order:Cart},
+        {headers}
+      );
+      alert(response.data.message);
+      navigate("/profile/orderHistory");
+    } catch(error){
+      console.log(error);
+    }
+  };
+
   return (
-  <>
-  {!Cart && <Loader/>} 
+  <div className="bg-zinc-900 px-12 h-screen py-8">
+  {!Cart && (
+    <div className="w-full h-[100%] flex items-center justify-center">
+      <Loader/>{" "}
+    </div>
+  )} 
     {Cart && Cart.length===0 &&( 
       <div className="h-screen">
         <div className="h-[100%] flex items-center justify-center flex-col ">
-          <h1 className="text-5xl lg:text-6xl font-semibold text-zinc-400 ">
+          <h1 className="text-5xl lg:text-6xl font-semibold text-zinc-400 "> 
             Empty Cart
           </h1>
           <img
@@ -19,6 +75,7 @@ const Cart=()=>{
         </div>
       </div>
     )}
+    
     {Cart && Cart.length>0 && (
       <>
         <h1 className="text-5xl font-semibold text-zinc-500 mb-8 ">
@@ -53,23 +110,40 @@ const Cart=()=>{
                  Rs. {items.price}
               </h2>
               <button
-                className="bg-red-100 text-"
-
+                className="bg-red-100 text-red-700 border border-red-700 rounded p-2 ms-12"
+                onClick={()=> deleteItem(items._id)}
               >
-                </>
+                <AiFillDelete/>
               </button>
             </div>
           </div>
 
         ))}
       </>
-    )}</>;
-  
+    )}
+    {Cart && Cart.length>0 &&(
+      <div className="mt-4 w-full flex items-center justify-end">
+        <div className="p-4 bg-zinc-800 rounded">
+          <h1 className="text-3xl text-zinc-200 font-semibold">
+            Total amount
+          </h1>
+          <div className="mt-3 flex items-center justify-between text-xl text-zinc-200">
+            <h2>{Cart.length} books</h2> <h2> Rs {Total}</h2>
+          </div>
+          <div className="w-[100%] mt-3">
+            <button className="bg-zinc-100 rounded px-4 py-2 flex justify-center w-full font-semibold hover:bg-zinc-"
+            onClick={PlaceOrder}
+            >
+              Place your order
+            </button>
 
-
-
-};
-
+          </div>
+        </div>
+      </div>
+    )}
+    </div>
+  );
+}
 export default Cart;
 
 
