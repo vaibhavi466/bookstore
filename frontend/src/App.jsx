aed319e485089ff5bdde5678666b8467a87341d7
unknown <pushpalata3083@gmail.com>
2025-08-03 00:29:50 +0530

    branch switch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,13 +13,8 @@ import Favourites from "./components/Profile/Favourites";
 import UserOrderHistory from "./components/Profile/UserOrderHistory";
 import Settings from "./components/Profile/Settings";
 import AllOrders from "./pages/AllOrders";
-import AddBook from "./pages/AddBook";
+import AddBook from "./pages/AddBook"
 import UpdateBook from "./pages/UpdateBook";
-<<<<<<< HEAD
-=======
-
-
->>>>>>> c68fe26bf7cd0a7d0745a5d7c0d2486c40c62b41
 
 function App() {
   const role = useSelector((state) => state.auth.role);
